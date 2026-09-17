ab2379d44efd968a10fe514723573f8ccfe94156
Megaruf Bilal K <bk2155453@gmail.com>
2026-09-17 23:57:33 +0530

    Changed The File Name

--- Entities/JWT_Service.cs
+++ Entities/JWT_Service.cs
@@ -1,6 +1,6 @@
 ﻿namespace Library_Management.Entities
 {
-    public class JWT_Response
+    public class JWT_Service
     {
         public string Email { get; set; }
         public string Token { get; set; }
